7dacd90373a9b115f6de0438b4833beb7bd51d31
AdrianoReidel <adrianojosereidel@hotmail.com>
2025-02-14 14:47:08 -0300

    criado primeiro esboço do layout com as imagens principais

--- portfolioadrianoreidel/src/App.js
+++ portfolioadrianoreidel/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.css';
+import images from './images';
 
 function App() {
   return (
     <div className="App">
-      <span>Em Produção ...</span>
+      <img
+        src={images.adrianoreidel}
+        alt="Adriano Reidel"
+        className="portfolio-image-nome"
+      />
+      <img
+        src={images.jrfullstacksoftwaredeveloper}
+        alt="JR Full Stack Software Developer"
+        className="portfolio-image-cargo"
+      />
+
+      <div className="image-row">
+        <img
+          src={images.craftingtable}
+          alt="craftingtablefundo"
+          className="portfolio-image-crafting"
+        />
+        <img
+          src={images.livrobranco}
+          alt="livrobranco"
+          className="portfolio-image-livro"
+        />
+      </div>
     </div>
   );
 }
---
+++ portfolioadrianoreidel/src/images.js
@@ -0,0 +1,8 @@
+const images = {
+    adrianoreidel: require('./adrianojosemazurekreidel.png'),
+    jrfullstacksoftwaredeveloper: require('./jrfullstacksoftwaredeveloper.png'),
+    craftingtable: require('./craftingtable.jpeg'),
+    livrobranco: require('./livrobranco.png'),
+  };
+  
+  export default images;
