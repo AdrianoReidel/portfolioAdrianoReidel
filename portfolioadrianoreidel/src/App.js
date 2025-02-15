673b985527ecbac4724fee5931e77e7dcdbd2fca
AdrianoReidel <adrianojosereidel@hotmail.com>
2025-02-15 16:32:21 -0300

    adicionando botões para o funcionamento da parte escrita no livro, e botões com links externos de perfis

--- portfolioadrianoreidel/src/App.js
+++ portfolioadrianoreidel/src/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import images from './images';
 
 function App() {
+  const [livroAtual, setLivroAtual] = useState(images.livrobranco);
+
+  const trocarImagem = (novaImagem) => {
+    setLivroAtual(novaImagem);
+  };
+
+   const handleNextPage = () => {
+    if (livroAtual === images.livrosobre) {
+      setLivroAtual(images.livrosobre2);
+    } else if (livroAtual === images.livroexperiencias) {
+      setLivroAtual(images.livroexperiencias2);
+    } else if (livroAtual === images.livroprojetos) {
+      setLivroAtual(images.livroprojetos2);
+    } else if (livroAtual === images.livrocontato) {
+      setLivroAtual(images.livrocontato2);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (livroAtual === images.livrosobre2) {
+      setLivroAtual(images.livrosobre);
+    } else if (livroAtual === images.livroexperiencias2) {
+      setLivroAtual(images.livroexperiencias);
+    } else if (livroAtual === images.livroprojetos2) {
+      setLivroAtual(images.livroprojetos);
+    } else if (livroAtual === images.livrocontato2) {
+      setLivroAtual(images.livrocontato);
+    }
+  };
+
   return (
     <div className="App">
       <img
@@ -22,11 +52,145 @@ function App() {
           alt="craftingtablefundo"
           className="portfolio-image-crafting"
         />
+
+        <div className="button-column">
+          <button
+            style={{
+              backgroundImage: `url(${images.sobre})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              width: '200px',
+              height: '40px',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            onClick={() => trocarImagem(images.livrosobre)}
+          />
+          <button
+            style={{
+              backgroundImage: `url(${images.experiencias})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              width: '200px',
+              height: '40px',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            onClick={() => trocarImagem(images.livroexperiencias)}
+          />
+          <button
+            style={{
+              backgroundImage: `url(${images.projetos})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              width: '200px',
+              height: '40px',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            onClick={() => trocarImagem(images.livroprojetos)}
+          />
+          <button
+            style={{
+              backgroundImage: `url(${images.contato})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              width: '200px',
+              height: '40px',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            onClick={() => trocarImagem(images.livrocontato)}
+          />
+          <button
+            style={{
+              backgroundImage: `url(${images.linkedin})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              width: '63px',
+              height: '45px',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            onClick={() => window.open('https://www.linkedin.com/in/adrianoreidel', '_blank', 'noopener,noreferrer')}
+          >
+          </button>
+
+          <button
+            style={{
+              backgroundImage: `url(${images.github})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              width: '63px',
+              height: '45px',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            onClick={() => window.open('https://github.com/AdrianoReidel', '_blank', 'noopener,noreferrer')}
+          >
+          </button>
+
+          <button
+            style={{
+              backgroundImage: `url(${images.email})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              width: '63px',
+              height: '45px',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            
+            onClick={() => window.location.href = 'mailto:adrianojosereidel@hotmail.com'}
+          >
+          </button>
+
+        </div>
+
         <img
-          src={images.livrobranco}
+          src={livroAtual}
           alt="livrobranco"
           className="portfolio-image-livro"
         />
+
+        <div className="button-column">
+          <button
+            style={{
+              backgroundImage: `url(${images.setadireita})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              width: '50px',
+              height: '50px',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            onClick={handleNextPage}
+          />
+          <button
+            style={{
+              backgroundImage: `url(${images.setaesquerda})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              width: '50px',
+              height: '50px',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            onClick={handlePrevPage}
+          />
+          <button
+            style={{
+              backgroundImage: `url(${images.pincel})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              width: '50px',
+              height: '50px',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            onClick={() => trocarImagem(images.livrobranco)}
+          />
+        </div>
       </div>
     </div>
   );
--- portfolioadrianoreidel/src/images.js
+++ portfolioadrianoreidel/src/images.js
@@ -3,6 +3,21 @@ const images = {
     jrfullstacksoftwaredeveloper: require('./jrfullstacksoftwaredeveloper.png'),
     craftingtable: require('./craftingtable.jpeg'),
     livrobranco: require('./livrobranco.png'),
+    sobre: require('./sobre.png'),
+    experiencias: require('./experiencias.png'),
+    contato: require('./contato.png'),
+    projetos: require('./projetos.png'),
+    livrosobre: require('./livrosobre.png'),
+    livrosobre2: require('./livrosobre2.PNG'),
+    livroexperiencias: require('./livroexperiencias.png'),
+    livroprojetos: require('./livroprojetos.png'),
+    livrocontato: require('./livrocontato.png'),
+    setadireita: require('./setadireita.png'),
+    setaesquerda: require('./setaesquerda.png'),
+    pincel: require('./pincel.png'),
+    linkedin: require('./linkedin.png'),
+    github: require('./github.png'),
+    email: require('./email.png'),
   };
   
   export default images;
