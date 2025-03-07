d7ebf0b4aced38fe546520f1ffcdd05696491d4a
AdrianoReidel <adrianojosereidel@hotmail.com>
2025-03-06 21:46:12 -0300

    mudar imagem fixa da crafting table para um background com os ícones clicáveis e com hover parte 2

--- portfolioadrianoreidel/src/App.js
+++ portfolioadrianoreidel/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import images from './images';
+import { tecnologias } from './tecnologias';
 
 function App() {
   const pages = [
@@ -42,99 +43,6 @@ function App() {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const tecnologias = [
-    { 
-      nome: "CSharp", 
-      icone: images.csharp, 
-      link: "https://dotnet.microsoft.com/pt-br/languages/csharp" 
-    },
-    { 
-      nome: "dotNetCore", 
-      icone: images.dotNetCore, 
-      link: "https://dotnet.microsoft.com/pt-br/apps/aspnet" 
-    },
-    { 
-      nome: "Postgre", 
-      icone: images.postgre, 
-      link: "https://www.postgresql.org/" 
-    },
-    { 
-      nome: "JavaScript", 
-      icone: images.javascript, 
-      link: "https://www.javascript.com/" 
-    },
-    { 
-      nome: "ExtJs", 
-      icone: images.extjs, 
-      link: "https://www.sencha.com/products/extjs/" 
-    },
-    { 
-      nome: "Sybase", 
-      icone: images.sybase, 
-      link: "https://www.sap.com/brazil/products/technology-platform/sql-anywhere.html" 
-    },
-    { 
-      nome: "GitHub", 
-      icone: images.git, 
-      link: "https://github.com/" 
-    },
-    { 
-      nome: "GitLab", 
-      icone: images.gitlab, 
-      link: "https://gitlab.com/gitlab-com" 
-    },
-    { 
-      nome: "NHibernate", 
-      icone: images.nhibernate, 
-      link: "https://nhibernate.info/" 
-    },
-    { 
-      nome: "Node.js", 
-      icone: images.nodejs, 
-      link: "https://nodejs.org" 
-    },
-    { 
-      nome: "Visual Studio",
-      icone: images.vs,          
-      link: "https://visualstudio.microsoft.com" 
-    },
-    { 
-      nome: "VS Code",      
-      icone: images.vscode,      
-      link: "https://code.visualstudio.com" 
-    },
-    { 
-      nome: "ReactJs.js", 
-      icone: images.nodejs, 
-      link: "https://react.dev/" 
-    },
-    { 
-      nome: "HTML", 
-      icone: images.htmlicon, 
-      link: "https://developer.mozilla.org/pt-BR/docs/Web/HTML" 
-    },
-    { 
-      nome: "CSS", 
-      icone: images.cssicon, 
-      link: "https://developer.mozilla.org/pt-BR/docs/Web/CSS" 
-    },
-    { 
-      nome: "Python", 
-      icone: images.python, 
-      link: "https://www.python.org/" 
-    },
-    { 
-      nome: "C", 
-      icone: images.c, 
-      link: "https://devdocs.io/c/" 
-    },
-    { 
-      nome: "Java", 
-      icone: images.java, 
-      link: "https://www.java.com/pt-BR/" 
-    },
-  ];
-
   return (
     <div className="App">
       <img
@@ -156,6 +64,20 @@ function App() {
                     <button 
                       key={index} 
                       onClick={() => window.open(tec.link, '_blank', 'noreferrer')}
+                    >
+                      <img 
+                        src={tec.icone} 
+                        alt={tec.nome} 
+                      />
+                    </button>
+                  ))}
+                </div>
+                <div className="button-list">
+                  {tecnologias.slice(9, 18).map((tec, index) => (
+                    <button
+                      key={index}
+                      onClick={() => window.open(tec.link, '_blank', 'noreferrer')}
+                      title={tec.nome}
                     >
                       <img 
                         src={tec.icone} 
@@ -314,8 +236,24 @@ function App() {
           <>
             <div className="image-row">
               <div className="craftingtable-container">
+                
                 <div className="button-grid">
                   {tecnologias.slice(0, 9).map((tec, index) => (
+                    <button
+                      key={index}
+                      onClick={() => window.open(tec.link, '_blank', 'noreferrer')}
+                      title={tec.nome}
+                    >
+                      <img 
+                        src={tec.icone} 
+                        alt={tec.nome} 
+                      />
+                    </button>
+                  ))}
+                </div>
+
+                <div className="button-list">
+                  {tecnologias.slice(9, 18).map((tec, index) => (
                     <button
                       key={index}
                       onClick={() => window.open(tec.link, '_blank', 'noreferrer')}
---
+++ portfolioadrianoreidel/src/tecnologias.js
@@ -0,0 +1,95 @@
+import images from './images';
+
+export const tecnologias = [
+    { 
+      nome: "CSharp", 
+      icone: images.csharp, 
+      link: "https://dotnet.microsoft.com/pt-br/languages/csharp" 
+    },
+    { 
+      nome: "dotNetCore", 
+      icone: images.dotNetCore, 
+      link: "https://dotnet.microsoft.com/pt-br/apps/aspnet" 
+    },
+    { 
+      nome: "Postgre", 
+      icone: images.postgre, 
+      link: "https://www.postgresql.org/" 
+    },
+    { 
+      nome: "JavaScript", 
+      icone: images.javascript, 
+      link: "https://www.javascript.com/" 
+    },
+    { 
+      nome: "ExtJs", 
+      icone: images.extjs, 
+      link: "https://www.sencha.com/products/extjs/" 
+    },
+    { 
+      nome: "Sybase", 
+      icone: images.sybase, 
+      link: "https://www.sap.com/brazil/products/technology-platform/sql-anywhere.html" 
+    },
+    { 
+      nome: "GitHub", 
+      icone: images.git, 
+      link: "https://github.com/" 
+    },
+    { 
+      nome: "GitLab", 
+      icone: images.gitlab, 
+      link: "https://gitlab.com/gitlab-com" 
+    },
+    { 
+      nome: "NHibernate", 
+      icone: images.nhibernate, 
+      link: "https://nhibernate.info/" 
+    },
+    { 
+      nome: "Node.js", 
+      icone: images.nodejs, 
+      link: "https://nodejs.org" 
+    },
+    { 
+      nome: "Visual Studio",
+      icone: images.vs,          
+      link: "https://visualstudio.microsoft.com" 
+    },
+    { 
+      nome: "VS Code",      
+      icone: images.vscode,      
+      link: "https://code.visualstudio.com" 
+    },
+    { 
+      nome: "ReactJs.js", 
+      icone: images.react, 
+      link: "https://react.dev/" 
+    },
+    { 
+      nome: "HTML", 
+      icone: images.htmlicon, 
+      link: "https://developer.mozilla.org/pt-BR/docs/Web/HTML" 
+    },
+    { 
+      nome: "CSS", 
+      icone: images.cssicon, 
+      link: "https://developer.mozilla.org/pt-BR/docs/Web/CSS" 
+    },
+    { 
+      nome: "Python", 
+      icone: images.python, 
+      link: "https://www.python.org/" 
+    },
+    { 
+      nome: "C", 
+      icone: images.c, 
+      link: "https://devdocs.io/c/" 
+    },
+    { 
+      nome: "Java", 
+      icone: images.java, 
+      link: "https://www.java.com/pt-BR/" 
+    },
+  ];
+  
